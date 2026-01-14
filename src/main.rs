3e56392f81a5c2da20dd31c5f1b6bffad05563ba
NicolasPllr1 <nicolas.pllr1@gmail.com>
2026-01-14 13:37:30 -0500

    Add docstring for the backward function

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,25 @@ use std::io::{Read, Write};
 
 trait Module {
     fn forward(&mut self, input: Array2<f32>) -> Array2<f32>; // Input is (batch_size, features)
+    /// Backward pass
+    ///
+    /// The `backward` function receives a gradient `dz` which corresponds to dLoss/dz,
+    /// where z is the current layer output.
+    /// The job of the backward pass is to combine its 'local gradients' - namely dz/dx, dz/dw -
+    /// with this incoming gradient - dLoss/dz - using the chain rule.
+    /// The backward should:
+    /// - return dLoss/dx = dz/dx * dLoss/dz
+    /// - fill in the gradients for the layer's own weights dLoss/dw = dz/dw * dLoss/dz
+    ///
+    /// Notice how both dLoss/dx (which will get returned) and dLoss/dw (which will get saved for later
+    /// optimization) are computed combining the incoming dLoss/dz and local gradient information.
+    /// The combination itself being specified by the chain rule.
+    ///
+    /// Note:
+    /// - the dz/dx Jacobian matrix is not materialized (too wasteful). Instead, each layer
+    /// directly computes the matrix-vector product of interest.
+    /// - the shape of the function output - which corresponds to dLoss/dx - is the same shape
+    /// as the layer inputs.
     fn backward(&mut self, dz: Array2<f32>) -> Array2<f32>;
     fn step(&mut self, learning_rate: f32);
 }
